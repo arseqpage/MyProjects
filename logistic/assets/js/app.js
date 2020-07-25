854ff0a231c2801c1498fe38d2e45bd806625f61
Arslan <64689183+arseqpage@users.noreply.github.com>
2020-07-25 22:46:14 +0300

    Logistic project complete

--- logistic/assets/js/app.js
+++ logistic/assets/js/app.js
@@ -132,7 +132,7 @@ $(function () {
                 transform: 'scale(1)',
                 opacity: '1'
             });
-        });
+        }, 100);
 
 
     });
@@ -180,9 +180,9 @@ $(function () {
         slidesToScroll: 1,
         arrows: false,
         fade: true,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 4000,
-        speed: 500
+        speed: 800
     });
 
     $("#introSliderPrev").on('click', function () {
@@ -207,8 +207,50 @@ $(function () {
         slidesToScroll: 1,
         arrows: false,
         dots: true,
-        speed: 500
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 4000,
     });
+
+
+    // AOS JS
+    // https://github.com/michalsnik/aos
+
+    AOS.init({
+        // Global settings:
+        disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 80, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 700, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 })
