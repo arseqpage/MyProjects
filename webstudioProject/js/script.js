3930c862d717d7903ffe027709476d5be92d38e0
Arslan <64689183+arseqpage@users.noreply.github.com>
2020-08-02 00:19:08 +0300

    webproject adaptive complete

--- webstudioProject/js/script.js
+++ webstudioProject/js/script.js
@@ -25,13 +25,51 @@ $(function () {
                 }
 
                 console.log(workCat);
-
-
             })
-
         }
+    });
+
+    // View All
+
+    function worksHide() {
+
+        let works = document.querySelectorAll('.works-item');
+
+        if (works.length > 6) {
+            for (let i = works.length - 1; i >= 6; i--) {
+                works[i].classList.add('hideTwo');
+            }
+        }
+    }
+
+    worksHide();
+
+    function show() {
+        let works = document.querySelectorAll('.works-item');
+        for (let i = works.length - 1; i >= 6; i--) {
+            works[i].classList.toggle('show');
+        }
+    }
+
+    document.querySelector('.works-view').onclick = show;
 
 
-    })
+    // Slick slider
+    //====================================
+
+    let reviewsSlider = $("#teamSlider");
+
+    reviewsSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 10000,
+    });
+
+
 
 })
