1017375c77393757368a92907edf3b64bdfed982
Arseqpage <arseqpage@gmail.com>
2020-08-28 22:23:35 +0300

    content

--- webstudioProject/js/script.js
+++ webstudioProject/js/script.js
@@ -1,5 +1,24 @@
 $(function () {
 
+    let navToggle = $('#navToggle');
+    let nav = $('#nav');
+
+
+    navToggle.on('click', function (event) {
+        event.preventDefault();
+
+        $("body").toggleClass('show-nav');
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+
+    });
+
+    $(window).on("resize", function () {
+        $("body").removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
+
 
     let filter = $("[data-filter]");
 
@@ -52,6 +71,61 @@ $(function () {
     }
 
     document.querySelector('.works-view').onclick = show;
+
+    /* Modal
+  ======================================*/
+
+    $('[data-modal]').on("click", function (event) {
+        event.preventDefault();
+
+        let modal = $(this).data('modal');
+        console.log(modal);
+
+        $('body').addClass('no-scroll');
+        $(modal).addClass('show');
+
+        //Анимация модального окна
+        setTimeout(function () {
+            $(modal).find('.modal__content').css({
+                transform: 'scale(1)',
+                opacity: '1'
+            });
+        }, 100);
+
+
+    });
+
+    $(' [data-modal-close]').on('click', function (event) {
+        event.preventDefault();
+
+        let modal = $(this).parents('.modal');
+
+        modalClose(modal);
+
+
+    });
+
+    $('.modal').on('click', function () {
+        let modal = $(this);
+
+        modalClose(modal);
+    });
+
+    $('.modal__content').on('click', function (event) {
+        event.stopPropagation(); //При клике на этот элемент не будет срабатывать событые клика по его родителю
+    })
+
+    function modalClose(modal) {
+        modal.find('.modal__content').css({
+            transform: 'scale(0.1)',
+            opacity: '0'
+        });
+
+        setTimeout(function () {
+            $('body').removeClass('no-scroll');
+            modal.removeClass('show');
+        }, 200);
+    }
 
 
     // Slick slider
